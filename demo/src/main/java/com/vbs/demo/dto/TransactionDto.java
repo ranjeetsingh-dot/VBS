fe10ba30e4a37f37772d1d58abf5f0d26aad7e2f
ranjeetsingh-dot <ranjeetsingh022005@gmail.com>
2025-12-30 18:59:48 +0530

    First Commit

---
+++ demo/src/main/java/com/vbs/demo/dto/TransactionDto.java
@@ -0,0 +1,17 @@
+package com.vbs.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionDto
+{
+    String username;
+    double amount;
+    int id;
+
+
+}
